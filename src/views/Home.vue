057f44b8918496b5a19707fb66cde0cc4e22bc00
Igor Muller <igormullersantos@hotmail.com>
2023-06-16 09:51:53 -0300

    Organizando arquivos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
-<template>
-  <HelloWorld />
-</template>
-
 <script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import Login from "@/components/Login.vue";
 </script>
+
+<template>
+  <Login />
+</template>
