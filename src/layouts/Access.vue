d7cb97831078b20fbe1e1eeb0a212d35ccd14ff8
Igor Muller <igormullersantos@hotmail.com>
2023-06-19 11:35:42 -0300

    Movendo alerta de usuário criado com sucesso para o layout

--- src/layouts/Access.vue
+++ src/layouts/Access.vue
@@ -1,7 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      showAlert: false,
+    };
+  },
+  methods: {
+    registered() {
+      this.showAlert = true;
+      setTimeout(() => {
+        this.showAlert = false;
+      }, 3000);
+    },
+  },
+};
+</script>
 <template>
   <v-app>
     <v-main class="bg-grey-lighten-2">
-      <router-view />
+      <div class="d-flex justify-center">
+        <v-alert
+          class="mt-4 w-100 justify-space-between elevation-15"
+          max-width="344"
+          position="fixed"
+          v-show="showAlert"
+          type="success"
+          title="User registered!"
+          text="Log in"
+          closable
+        >
+        </v-alert>
+      </div>
+
+      <router-view @registered="registered" />
     </v-main>
   </v-app>
 </template>
