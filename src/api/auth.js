b3eff2e14f02625340da547a3c686b74db20b8cf
Igor Muller <igormullersantos@hotmail.com>
2023-06-22 14:20:02 -0300

    Criando método de login base

--- src/api/auth.js
+++ src/api/auth.js
@@ -6,5 +6,9 @@ export const authAPiMixin = {
       const res = await api.post("/auth/register", payload);
       return res;
     },
+    async login(payload) {
+      const res = await api.post("/auth/login", payload);
+      return res;
+    },
   },
 };
