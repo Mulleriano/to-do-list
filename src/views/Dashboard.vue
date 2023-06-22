8c3ed8c4d4be3b09e03d942bc22f2e521980806a
Igor Muller <igormullersantos@hotmail.com>
2023-06-22 16:00:55 -0300

    Cria método para trazer a lista de ToDo no dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,23 @@
-<script setup>
+<script>
 import ToDo from "@/components/ToDo.vue";
+import { toDoListsApiMixin } from "@/api/toDoList";
+
+export default {
+  mixins: [toDoListsApiMixin],
+  methods: {
+    async getLists() {
+      try {
+        const { data } = await this.list();
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  mounted() {
+    this.getLists();
+  },
+};
 </script>
 
 <template>
