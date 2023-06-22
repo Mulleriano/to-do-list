25bfe6c08b30648ba491c91924ded1164a2fe82c
Igor Muller <igormullersantos@hotmail.com>
2023-06-22 15:57:08 -0300

    Dividindo apis em publica e privada

--- src/api/index.js
+++ src/api/index.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
-export const api = axios.create({
+export const publicApi = axios.create({
   baseURL: "http://localhost:3333",
   headers: new Headers({
     "Content-Type": "application/json",
   }),
 });
+export let privateApi;
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access_token");
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
-  }
-  return config;
-});
+export const setupPrivateApi = (token) => {
+  privateApi = axios.create({
+    baseURL: "http://localhost:3333",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
