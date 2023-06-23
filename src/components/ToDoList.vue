fa6e00b90742d3ee57620216458d972cdbc7bca3
Igor Muller <igormullersantos@hotmail.com>
2023-06-23 10:33:31 -0300

    Alimentando componente

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  props: {
+    toDoList: Array,
+  },
+};
+</script>
+<template>
+  <div v-for="toDo in toDoList" :key="toDo.id">
+    {{ toDo.title }}
+  </div>
+</template>
