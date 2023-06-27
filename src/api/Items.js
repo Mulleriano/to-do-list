87d64ae450835381c6492998f1aa6c6895a3c0a3
Emmanuelle <emmanuellematos9@gmail.com>
2023-06-27 17:18:43 -0300

    chamadas para deletar, editar e atualizar os itens

--- src/api/Items.js
+++ src/api/Items.js
@@ -5,5 +5,14 @@ export const itemsApiMixin = {
     async createItem(item) {
       return await privateApi.post("/items/new", item);
     },
+    async deleteItem(id) {
+      return await privateApi.delete(`/items/${id}`)
+    },
+    async editItem(id, payload) {
+      return await privateApi.patch(`/items/${id}`, payload)
+    },
+    async updateItem(id, payload) {
+      return await privateApi.patch(`/items/${id}`, payload);
+    },
   }, 
 };
