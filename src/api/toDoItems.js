2d368defe6589fdf5d1871b3e44151c6c8531daa
Igor Muller <igormullersantos@hotmail.com>
2023-06-29 14:43:19 -0300

    Renomeando arquivo

--- src/api/toDoItems.js
+++ src/api/toDoItems.js
@@ -1,15 +1,15 @@
 import { privateApi } from "./index";
 
-export const itemsApiMixin = {
+export const toDoItemsApiMixin = {
   methods: {
     async createItem(item) {
       return await privateApi.post("/items/new", item);
     },
     async removeItem(id) {
-      return await privateApi.delete(`/items/${id}`)
+      return await privateApi.delete(`/items/${id}`);
     },
     async updateItem(id, payload) {
       return await privateApi.patch(`/items/${id}`, payload);
     },
-  }, 
+  },
 };
