a84674ecd8a3dc7cf1847117f0a4b9d2c1cb5cec
Igor Muller <igormullersantos@hotmail.com>
2023-06-30 14:36:57 -0300

    Importando os componentes para o dashboard e removendo código antigo

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,8 @@
 <script>
 import ToDoList from "@/components/ToDoList.vue";
+import CreateModal from "@/components/CreateModal.vue";
+import UpdateModal from "@/components/UpdateModal.vue";
+import RemoveModal from "@/components/RemoveModal.vue";
 import { toDoListsApiMixin } from "@/api/toDoList";
 import handleAlertMixin from "@/mixins/handleAlert";
 import loadingMixin from "@/mixins/loading";
@@ -7,14 +10,15 @@ import loadingMixin from "@/mixins/loading";
 export default {
   components: {
     ToDoList,
+    UpdateModal,
+    RemoveModal,
+    CreateModal,
   },
   data() {
     return {
       showCreate: false,
       showRemove: false,
       showUpdate: false,
-      createTitle: "",
-      updateTitle: "",
       selectedTitle: "",
       selectedId: "",
       toDoList: [],
@@ -31,13 +35,13 @@ export default {
       }
     },
 
-    async createList() {
+    async createList(title) {
       try {
         this.loadingCreate = true;
-        const title = {
-          title: this.createTitle,
+        const payload = {
+          title: title,
         };
-        const { data } = await this.create(title);
+        const { data } = await this.create(payload);
         this.toDoList.push(data);
         this.createAlert();
       } catch (err) {
@@ -49,10 +53,10 @@ export default {
         this.showCreate = false;
       }
     },
-    async removeList(id) {
+    async removeList() {
       try {
         this.loadingRemove = true;
-        await this.remove(id);
+        await this.remove(this.selectedId);
         this.deleteAlert();
         this.getLists();
       } catch (err) {
@@ -64,13 +68,13 @@ export default {
       }
     },
 
-    async updateList(id) {
+    async updateList(title) {
       try {
-        this.loadingUpdate = true;
-        const title = {
-          title: this.updateTitle,
+        const payload = {
+          title: title,
         };
-        await this.update(id, title);
+        await this.update(this.selectedId, payload);
+        this.selectedTitle = "";
         this.getLists();
       } catch (err) {
         const message = err.message;
@@ -90,9 +94,12 @@ export default {
 
     startUpdate(toDo) {
       this.showUpdate = true;
-      this.updateTitle = toDo.title;
+      this.selectedTitle = toDo.title;
       this.selectedId = toDo.id;
       if (this.showRemove) this.showRemove = false;
+    },
+    closeUpdate() {
+      this.showUpdate = false;
     },
   },
   mounted() {
@@ -102,33 +109,9 @@ export default {
 </script>
 
 <template>
-  <v-dialog class="w-50 d-flex align-center" v-model="showCreate">
-    <v-card color="grey-darken-4" width="auto" class="pa-6 d-flex align-center">
-      <v-card-title class="py-4 text-h5">Create List</v-card-title>
-      <v-form @submit.prevent="createList">
-        <v-text-field
-          v-model="createTitle"
-          label="List title"
-          variant="underlined"
-          clearable
-        ></v-text-field>
-        <v-card-actions>
-          <v-btn @click="showCreate = !showCreate">Close</v-btn>
-          <v-btn
-            class="bg-green-accent-2"
-            :disabled="createTitle === ''"
-            @click="createList()"
-            :loading="loadingCreate"
-            >Create</v-btn
-          >
-        </v-card-actions>
-      </v-form>
-    </v-card>
-  </v-dialog>
-
   <v-sheet
     color="transparent"
-    class="mb-6"
+    class="ma-6"
     style="z-index: 20001"
     position="fixed"
     location="bottom"
@@ -175,43 +158,23 @@ export default {
     </v-card>
   </v-sheet>
 
-  <v-dialog v-model="showRemove" class="w-50 d-flex align-center">
-    <v-card color="grey-darken-4" width="auto" class="pa-6 d-flex align-center">
-      <v-card-title class="text-h5">Delete</v-card-title>
-      <v-card-text
-        >Are you shure you want to delete '{{ selectedTitle }}'?</v-card-text
-      >
-      <v-card-actions>
-        <v-btn @click="showRemove = !showRemove">Close</v-btn>
-        <v-btn
-          class="bg-error"
-          :loading="loadingRemove"
-          @click="removeList(selectedId)"
-          >Delete</v-btn
-        >
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <create-modal
+    :showCreate="showCreate"
+    @close="showCreate = false"
+    @save="createList"
+  />
 
-  <v-dialog class="w-50 d-flex align-center" v-model="showUpdate">
-    <v-card color="grey-darken-4" width="auto" class="pa-6 d-flex align-center">
-      <v-card-title class="text-h5 py-4">Edit</v-card-title>
-      <v-form @submit.prevent="updateList(selectedId)">
-        <v-text-field
-          class="w-100"
-          v-model="updateTitle"
-          variant="underlined"
-        ></v-text-field>
-        <v-card-actions>
-          <v-btn @click="showUpdate = !showUpdate">close</v-btn>
-          <v-btn
-            class="bg-green-accent-2"
-            @click="updateList(selectedId)"
-            :loading="loadingUpdate"
-            >Save</v-btn
-          >
-        </v-card-actions>
-      </v-form>
-    </v-card>
-  </v-dialog>
+  <remove-modal
+    :showRemove="showRemove"
+    :selectedTitle="selectedTitle"
+    @close="showRemove = false"
+    @remove="removeList"
+  />
+
+  <update-modal
+    :showUpdate="showUpdate"
+    :selectedTitle="selectedTitle"
+    @close="showUpdate = false"
+    @save="updateList"
+  />
 </template>
