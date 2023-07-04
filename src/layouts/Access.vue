0200db5c7d14e9c9e842d8504f588b9f08c0fef0
Igor Muller <igormullersantos@hotmail.com>
2023-07-04 11:22:30 -0300

    Ajustando layout de acesso no celular

--- src/layouts/Access.vue
+++ src/layouts/Access.vue
@@ -26,11 +26,18 @@ export default {
       }, 2000);
     },
   },
+  computed: {
+    height() {
+      return {
+        "h-auto": this.$vuetify.display.smAndDown,
+      };
+    },
+  },
 };
 </script>
 <template>
   <v-app>
-    <v-main class="bgGreen d-flex flex-column justify-center align-center">
+    <v-main class="bgGreen d-flex flex-column justify-sm-center align-center">
       <div>
         <v-fade-transition>
           <v-alert
@@ -52,23 +59,25 @@ export default {
         color="white"
         max-width="800"
         height="550"
-        class="rounded-lg mx-8 d-flex justify-left flex-column flex-sm-row align-center elevation-15"
+        class="rounded-lg mx-8 my-8 d-flex justify-left flex-column flex-sm-row align-sm-center elevation-15"
+        :class="height"
       >
-        <div
-          class="bg-grey-darken-4 h-100 w-100 pa-8 d-flex flex-column align-center justify-center rounded-s-lg"
+        <v-card
+          color="grey-darken-4"
+          class="rounded-lg h-100 w-100 pa-8 d-flex flex-column align-center justify-space-around elevation-0"
         >
-          <h1 class="text-green-accent-2">TO DO LIST</h1>
+          <h1 class="text-green-accent-2 ma-4">TO DO LIST</h1>
           <lottie-player
             src="https://assets9.lottiefiles.com/packages/lf20_KWqh4bY8YV.json"
             background="transparent"
             speed="1"
-            style="width: 100%; height: auto"
+            style="width: 80%; height: auto"
             loop
             autoplay
           ></lottie-player>
-        </div>
+        </v-card>
 
-        <v-sheet position="relative" class="w-auto ma-8">
+        <v-sheet class="w-auto ma-8">
           <router-view @showAlert="showAlert" />
         </v-sheet>
       </v-sheet>
